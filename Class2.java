36001b7ab2502a6470aa4dc9290ee5bb2aa091bc
Rajat Petwal <43837760+rpetwal@users.noreply.github.com>
2019-07-03 17:14:35 +0530

    Add files via upload
    third commit

--- Class2.java
+++ Class2.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 /*
 Date - 02 July 2019
@@ -12,11 +13,12 @@ Author - rpetwal
 
 class Class2 extends JFrame implements ActionListener
 {
-    int highscore;
+    int highscore,number,chance = 7;
     JButton B;
-    JTextField T1,T2;
-    JLabel L1,L2,L3;
+    JTextField T1,T2,T3;
+    JLabel L1,L2,L3,L4,L5,L6;
     String diff;
+    Random rand = new Random();
     Class2(String diff)
     {
         this.diff = diff;
@@ -26,25 +28,76 @@ class Class2 extends JFrame implements ActionListener
         B = new JButton("OK");
         T1 = new JTextField(3);
         T2 = new JTextField(8);
+        T3 = new JTextField(1);
         L1 = new JLabel("Enter number : ");
         L1.setVerticalAlignment(SwingConstants.CENTER);
         L2 = new JLabel("<html></br>Result :</html>");
         L3 = new JLabel("<html><p1>"+this.diff+" mode <br/></p1></html>");
         L3.setHorizontalAlignment(SwingConstants.CENTER);
         L3.setVerticalAlignment(SwingConstants.TOP);
+        L4 = new JLabel("Chances : ");
+        T3.setText(chance+"");
+        L5 = new JLabel("Your score :");
         add(L3);
+        add(L4);
+        add(T3);
         add(L1);
         add(T1);
         add(L2);
         add(T2);
         add(B);
         setVisible(true);
+        if(diff.equals("Easy"))
+           number = rand.nextInt(10);
+        else if(diff.equals("Medium"))
+            number = rand.nextInt(50);
+        else
+            number = rand.nextInt(100);
+        B.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e)
+                {
+                       int ent_num;
+                    String text = T1.getText();
+                    ent_num = Integer.parseInt(text);
+                    if(chance == 1)
+                    {
+                        T2.setText("Lose!");
+                        highscore = 0;
+                        L5 = new JLabel(highscore+"");
+                        add(L5);
+                        add(L6);
+                        setVisible(true);
+                    }
+                else{
+                    if(ent_num == number){
+                        T2.setText("Correct!");
+                        highscore = chance * 100;
+                        L6 = new JLabel(highscore+"");
+                        add(L5);
+                        add(L6);
+                        setVisible(true);
+                        
+                    }
+                    else if(ent_num > number)
+                    {
+                        T2.setText("Greater");
+                        chance--;
+                    }
+                    else
+                    {
+                        T2.setText("Smaller");
+                        chance--;
+                    }   
+                    }
+                    T3.setText(chance+"");
+                }
+        });
     }
     
     
     @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void actionPerformed(ActionEvent e){
+        
     }
-    
 }
+    
